ee6f0153173a7875fc65cc8461e82f16bd2fad1d
bob881688 <sunveronica1666@gmail.com>
2026-02-13 02:02:57 +0800

    新增開發用middleCORS

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 
 from database import init_resources, close_resources
@@ -19,6 +20,15 @@ async def app_lifespan(app: FastAPI):
         # Shutdown 階段
         await close_resources()
 
+
 app = FastAPI(lifespan=app_lifespan)
 
+# 開發用
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(data_router, prefix="/data")
